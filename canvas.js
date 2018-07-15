7681ebd0852148614087219d4dbc86d2a6a8e250
hinch5 <o.shatnuk@gmail.com>
2018-07-15 16:39:25 +0300

    #9 функции, двигающие канвасы на передний/задний план

--- canvas.js
+++ canvas.js
@@ -1,44 +1,54 @@
 'use strict';
 
 class Canvas {
-	clicked=false;
+	clicked = false;
 	clickedX;
 	clickedY;
 	drawFunction;       // функция принимает ссылку на канвас и евент. Рисует что-то определенное и для всех инструментов разная
 	frontCanvas;        // ссылка на "передний" канвас. Нужна, чтобы отрисовывать изменения на него по окончании работы с нижним
 	element;
 	context;
-	constructor(id,frontCanvas) {
-		this.element=document.getElementById(id);
-		this.context=this.element.getContext('2d');
-		this.frontCanvas=frontCanvas;
+
+	constructor(id, frontCanvas) {
+		this.element = document.getElementById(id);
+		this.context = this.element.getContext('2d');
+		this.frontCanvas = frontCanvas;
 		this.element.onmousedown = (ev => {
-			this.clicked=true;
-			this.clickedX=ev.offsetX;
-			this.clickedY=ev.offsetY;
+			this.clicked = true;
+			this.clickedX = ev.offsetX;
+			this.clickedY = ev.offsetY;
 		});
 		this.element.onmousemove = (ev => {
-			if (this.clicked){
+			if (this.clicked) {
 				/* очистка, необходимая только для заднего канваса. Нужна в ситуациях типа рисования прямоугольника:
 				   нужно отображать, какой прямоугольник получается и при этом стирать старые изображения
 				 */
-				if (this.frontCanvas!=null){
-					this.context.clearRect(0,0,this.element.offsetWidth,this.element.offsetHeight);
+				if (this.frontCanvas != null) {
+					this.context.clearRect(0, 0, this.element.offsetWidth, this.element.offsetHeight);
 				}
-				this.drawFunction(this,ev);
+				this.drawFunction(this, ev);
 			}
 		});
 		this.element.onmouseup = (ev => {
-			if (this.frontCanvas!=null){
-				this.context.clearRect(0,0,this.element.offsetWidth,this.element.offsetHeight);
-				this.drawFunction(this.frontCanvas,ev);
-			} else{
-				this.drawFunction(this,ev);
+			if (this.frontCanvas != null) {
+				this.context.clearRect(0, 0, this.element.offsetWidth, this.element.offsetHeight);
+				this.drawFunction(this.frontCanvas, ev);
+			} else {
+				this.drawFunction(this, ev);
 			}
 		});
 		this.element.onmouseleave = this.element.onmouseup;
 	}
-	set drawer(d){
-		this.drawFunction=d;
+
+	set drawer(d) {
+		this.drawFunction = d;
+	}
+
+	makeFirst() {
+		this.element.style.zIndex = "2";
+	}
+
+	makeSecond() {
+		this.element.style.zIndex = "1";
 	}
 }
